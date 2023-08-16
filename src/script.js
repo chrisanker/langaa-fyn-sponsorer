8942778ad17f635b2250e83177ea585fefd5913e
Christian Anker Larsen <calanker@gmail.com>
2023-08-16 21:32:43 +0200

    added more images and moved elements around

--- src/script.js
+++ src/script.js
@@ -25,8 +25,20 @@ const images = [{
     alt: "Østfyn VVS og Energiservice"
 },
 {
-    url: "https://static.wixstatic.com/media/d4c8cd_f8d7a41f8eaf4dbda0b5c172af634f66~mv2.jpg",
+    url: "https://static.wixstatic.com/media/a89772_0d87546ae24c4b34ac27196f8cd7dadc~mv2.jpg",
     alt: "Hesselager Bilpleje"
+},
+{
+    url: "https://static.wixstatic.com/media/a89772_3fc7cf06ea8b48d5be4ba4015ffbaa52~mv2.jpg",
+    alt: "Carsten Andersen ApS Murermester"
+},
+{
+    url: "https://static.wixstatic.com/media/a89772_67ce54145cc54e33b384bdd35e32a921~mv2.jpg",
+    alt: "Ellerup Tømrerforretning"
+},
+{
+    url: "https://static.wixstatic.com/media/a89772_687e3ac67b1c490a8d50f31ca99a25ff~mv2.jpg",
+    alt: "Højlund Mølle - Ryttershoppen"
 }];
 
 
@@ -38,10 +50,26 @@ function createImg() {
 function nextImg() {
     imageOnDisplay.setAttribute("src", images[currentImg].url);
     imageOnDisplay.setAttribute("alt", images[currentImg].alt);
-    if(currentImg + 1 >= images.length) {
+    if (currentImg + 1 >= images.length) {
         currentImg = 0;
     }
     else {
         currentImg++;
     }
 }
+
+/* const statusMessage = document.querySelector("#statusMessage");
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const imageUrl = formData.get("url");
+    const imageAlt = formData.get("alt");
+    images.push({url: imageUrl, alt: imageAlt})
+    statusMessage.textContent = "Url successfully saved."
+    let i;
+    for (i = 0; i < images.length; i++) {
+        console.log(images[i]);
+    }
+}); */
