59dc625349c6f1489f262d9014894f1fc671f7b7
Christian Anker Larsen <calanker@gmail.com>
2023-09-08 22:47:17 +0200

    Changed implementation to set display property instead of element attributes

--- src/script.js
+++ src/script.js
@@ -1,11 +1,20 @@
 const imageOnDisplay = document.querySelector("#imageOnDisplay");
 
-let currentImg = 1;
+let slideIndex = 0;
 
-window.onload = (event) => {
-    createImg();
-    const millisBetweenImgChange = 5000;
-    setInterval(nextImg, millisBetweenImgChange);
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("slides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    if (slideIndex >= slides.length) {slideIndex = 0}
+    slides[slideIndex].style.display = "inline-block";
+    slides[slideIndex].classList.add("image");
+    slideIndex++;
+    setTimeout(showSlides, 5000);
 }
 
 const images = [{
